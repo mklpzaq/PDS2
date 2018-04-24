4d101f3174a44ab10c4371969081fd938453c9da
smart-202-5 <smart-202-5@smart-202-5-PC>
2018-04-24 13:52:35 +0900

    [임가현] 주석수정

--- PDS2/src/main/java/com/test/pds2/notice/service/Notice.java
+++ PDS2/src/main/java/com/test/pds2/notice/service/Notice.java
@@ -4,7 +4,7 @@ public class Notice {
 	private int noticeId; // notice_id
 	private String noticeTitle; //notice_title
 	private String noticeContent; //notice_content
-	private NoticeFile noticeFile; // or private ArticleFile[] articleFile; 현재 1:1로 구성, 나중에 1:N으로 바꿔야함, command객체
+	private NoticeFile noticeFile; //현재 1:1로 구성, 나중에 1:N으로 바꿔야함, command객체
 	
 	public int getNoticeId() {
 		return noticeId;
